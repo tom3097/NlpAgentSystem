2b41ca48b3ed029324254779e7408a6e3893bf87
Tomasz Bochenski <T.Bochenski@stud.elka.pw.edu.pl>
2018-06-04 15:51:38 +0200

    scripts to run nginx on local machine

---
+++ nginx/local_start_nginx.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo nginx -c local_nginx.conf
---
+++ nginx/local_update_app.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sudo cp -r static/ /usr/local/Cellar/nginx/1.13.12/html/
+sudo cp local_nginx.conf /usr/local/Cellar/nginx/1.13.12/
